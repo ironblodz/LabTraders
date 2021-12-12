afd802c5ef6594cd7fc9db998464568f2789f4f9
ironblodz <naylestorm12@gmail.com>
2021-12-12 11:57:52 +0000

    update

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,20 +6,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8" />
-   
+
     <title>@yield('title')</title>
     <script src="{{ asset('js/main.js') }}"></script>
-      
+
         <title>Knight Bootstrap Template - Index</title>
         <meta content="" name="description">
         <meta content="" name="keywords">
-      
+
         <!-- Favicons -->
         <link href="{{asset('img/favicon.png')}}" rel="icon">
         <link rel="stylesheet" href="{{asset('css/style.css') }}">
         <!-- Google Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-      
+
         <!-- Vendor CSS Files -->
         <link href="{{asset('vendor/aos/aos.css')}}" rel="stylesheet">
         <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
@@ -27,16 +27,16 @@
         <link href="{{asset('vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
         <link href="{{asset('vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
         <link href="{{asset('vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
-      
+
         <!-- Template Main CSS File -->
         <link href="assets/css/style.css" rel="stylesheet">
-  
+
 </head>
 
 <body>
     <section id="hero">
         <div class="hero-container">
-          <a href="index.html" class="hero-logo" data-aos="zoom-in"><img src="{{asset('img/favicon.png')}}" alt=""></a>
+          <a href="index.html" class="hero-logo" data-aos="zoom-in"><img src="#" alt=""></a>
           <h1 data-aos="zoom-in">Bem-vindo</h1>
           <h2 data-aos="fade-up">Análise Técnica / Psicologia de Mercado / Gestão do Capital </h2>
           <h2 data-aos="fade-up">Forex / Criptomoedas / Ações </h2>
@@ -50,7 +50,7 @@
 
     @include('layout.partials.footer')
 
-    
+
   <!-- Vendor JS Files -->
   <script src="{{asset('vendor/aos/aos.js')}}"></script>
   <script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
