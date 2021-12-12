feeea3885c50c0c5571ab7414d33f8a896c63a82
ironblodz <naylestorm12@gmail.com>
2021-12-12 01:54:48 +0000

    primeiro commit

---
+++ resources/views/layout/partials/navbar.blade.php
@@ -0,0 +1,28 @@
+<header id="header" class="d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <div class="logo">
+        <a href="{{route('gm.index')}}"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li>
+            <a class="nav-link scrollto active" href="{{route('gm.index')}}">Home</a>
+        </li>
+          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li><a class="nav-link scrollto" href="#services">Services</a></li>
+          <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
+          <li><a class="nav-link scrollto" href="#team">Team</a></li>
+          <li><a class="nav-link scrollto" href="#pricing">Pricing</a></li>
+          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a></li>
+          <li>
+            <a class="nav-link scrollto" href="{{route('gm.contact')}}">Contact</a>
+        </li>
+        
+        <i class="bi bi-list mobile-nav-toggle"></i>
+        </ul>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
---
+++ resources/views/master.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+   
+    <title>@yield('title')</title>
+    <script src="{{ asset('js/main.js') }}"></script>
+      
+        <title>Knight Bootstrap Template - Index</title>
+        <meta content="" name="description">
+        <meta content="" name="keywords">
+      
+        <!-- Favicons -->
+        <link href="{{asset('img/favicon.png')}}" rel="icon">
+        <link href="{{asset('img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+        <link rel="stylesheet" href="{{asset('css/style.css') }}">
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+      
+        <!-- Vendor CSS Files -->
+        <link href="{{asset('vendor/aos/aos.css')}}" rel="stylesheet">
+        <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+        <link href="{{asset('vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+        <link href="{{asset('vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+        <link href="{{asset('vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+        <link href="{{asset('vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+      
+        <!-- Template Main CSS File -->
+        <link href="assets/css/style.css" rel="stylesheet">
+  
+</head>
+
+<body>
+    @include('layout.partials.navbar')
+
+    @yield('content')
+
+    @include('layout.partials.footer')
+
+    
+  <!-- Vendor JS Files -->
+  <script src="{{asset('vendor/aos/aos.js')}}"></script>
+  <script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{asset('vendor/glightbox/js/glightbox.min.js')}}"></script>
+  <script src="{{asset('vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+  <script src="{{asset('vendor/php-email-form/validate.js')}}"></script>
+  <script src="{{asset('vendor/swiper/swiper-bundle.min.js')}}"></script>
+
+  <!-- Template Main JS File -->
+  <script src="{{asset('js/main.js')}}"></script>
+</body>
+
+
+</html>
